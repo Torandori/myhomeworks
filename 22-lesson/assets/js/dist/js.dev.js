fc190c2029d43601edddd3ca254d40be69f5f2fb
Torandori <vaviktova@gmail.com>
2023-05-27 01:44:15 +0200

    Added les 22

--- 22-lesson/assets/js/dist/js.dev.js
+++ 22-lesson/assets/js/dist/js.dev.js
@@ -15,26 +15,24 @@ function userAge() {
     document.getElementById("task_1_result").innerHTML = "<i style=\"color: red\">Enter value from 0 till 100</i>";
     return;
   }
-}
+} // function symbol() {
+//   const age = document.getElementById("task_1_number").valueAsNumber;
+//   if(!isNaN(age) && age < 12 && age > 0 && age !== 0 ) {
+//     document.getElementById("task_1_result").innerHTML = `<i style="color: green">Child</i>`;
+//   } else if (age >= 12 && age < 18) {
+//     document.getElementById("task_1_result").innerHTML = `<i style="color: green">Teenager</i>`;
+//   } else if (age >= 18 && age < 60) {
+//     document.getElementById("task_1_result").innerHTML = `<i style="color: green">Adult</i>`;
+//   } else if (age >=60 && age <= 100) {
+//     document.getElementById("task_1_result").innerHTML = `<i style="color: green">Senior</i>`;
+//   } else {
+//     document.getElementById("task_1_result").innerHTML = `<i style="color: red">Enter value from 0 till 100</i>`;
+//     return ; 
+//   }
+// }
+
 
 function symbol() {
-  var age = document.getElementById("task_1_number").valueAsNumber;
-
-  if (!isNaN(age) && age < 12 && age > 0 && age !== 0) {
-    document.getElementById("task_1_result").innerHTML = "<i style=\"color: green\">Child</i>";
-  } else if (age >= 12 && age < 18) {
-    document.getElementById("task_1_result").innerHTML = "<i style=\"color: green\">Teenager</i>";
-  } else if (age >= 18 && age < 60) {
-    document.getElementById("task_1_result").innerHTML = "<i style=\"color: green\">Adult</i>";
-  } else if (age >= 60 && age <= 100) {
-    document.getElementById("task_1_result").innerHTML = "<i style=\"color: green\">Senior</i>";
-  } else {
-    document.getElementById("task_1_result").innerHTML = "<i style=\"color: red\">Enter value from 0 till 100</i>";
-    return;
-  }
-}
-
-function symbol1() {
   var number = document.getElementById("task_2_number").valueAsNumber;
 
   switch (number) {
@@ -83,7 +81,43 @@ function symbol1() {
   }
 }
 
-function countDivider() {
+function sum() {
+  var num = document.getElementById("task_3_number").valueAsNumber;
+  var num1 = document.getElementById("task_3_number1").valueAsNumber;
+  var result = 0;
+
+  if (isNaN(num)) {
+    document.getElementById("task_3_result").innerHTML = "<i style=\"color: red\">Enter value</i>";
+    return false;
+  }
+
+  for (var i = num; i <= num1; i++) {
+    result += i;
+  }
+
+  document.getElementById("task_3_result").innerHTML = "<i style=\"color: red\">".concat(result, "</i>");
+}
+
+function commonDivisor() {
+  var num = document.getElementById("task_4_number").valueAsNumber;
+  var num1 = document.getElementById("task_4_number1").valueAsNumber;
+  var result = 0;
+
+  if (isNaN(num)) {
+    document.getElementById("task_4_result").innerHTML = "<i style=\"color: red\">Enter a value</i>";
+    return false;
+  }
+
+  for (var i = 1; i <= num && i <= num1; i++) {
+    if (num % i === 0 && num1 % i === 0) {
+      result = i;
+    }
+  }
+
+  document.getElementById("task_4_result").innerHTML = "<i style=\"color: red\">".concat(result, "</i>");
+}
+
+function countDivisor() {
   var num = document.getElementById("task_5_number").valueAsNumber;
   var result = '';
 
@@ -100,4 +134,122 @@ function countDivider() {
 
   result += num;
   document.getElementById("task_5_result").innerHTML = "<i style=\"color: red\">".concat(result, "</i>");
+}
+
+function palindrome() {
+  var num = document.getElementById("task_1_normal_number").valueAsNumber;
+  var result = '';
+
+  if (isNaN(num)) {
+    document.getElementById("task_1_normal_result").innerHTML = "<i style=\"color: red\">Enter five-digit value</i>";
+    return false;
+  }
+
+  if (Math.trunc(num / 10000) === num % 10 && Math.trunc(num / 1000 % 10) === Math.trunc(num % 100 / 10)) {
+    document.getElementById("task_1_normal_result").innerHTML = "<i style=\"color: green\">The digit is palindrome</i>";
+  } else {
+    document.getElementById("task_1_normal_result").innerHTML = "<i style=\"color: red\">The digit is NOT palindrome</i>";
+  }
+}
+
+function beforeDiscount() {
+  var amount = document.getElementById("task_2_normal_number").valueAsNumber;
+  var result = '';
+
+  if (isNaN(amount)) {
+    document.getElementById("task_2_normal_result").innerHTML = "<i style=\"color: red\">Enter a value</i>";
+    return false;
+  }
+
+  if (amount >= 200 && amount <= 300) {
+    result = (amount / (1 - 0.03)).toFixed(2);
+  } else if (amount > 300 && amount <= 500) {
+    result = (amount / (1 - 0.05)).toFixed(2);
+  } else if (amount > 500) {
+    result = (amount / (1 - 0.07)).toFixed(2);
+  }
+
+  document.getElementById("task_2_normal_result").innerHTML = "<i style=\"color: green\">The amount without discount is ".concat(result, " USD</i>");
+}
+
+function countNumbers() {
+  var i = 0;
+  var positiveCount = 0;
+  var negativeCount = 0;
+  var zeroCount = 0;
+  var evenCount = 0;
+  var oddCount = 0;
+
+  while (i < 10) {
+    var number = parseInt(prompt("Enter number ".concat(i + 1, ":")));
+
+    if (isNaN(number)) {
+      alert("Invalid number. Please try again.");
+      continue;
+    }
+
+    if (number > 0) {
+      positiveCount++;
+    } else if (number < 0) {
+      negativeCount++;
+    } else {
+      zeroCount++;
+    }
+
+    if (number % 2 === 0) {
+      evenCount++;
+    } else {
+      oddCount++;
+    }
+
+    i++;
+  }
+
+  document.getElementById("task_4_normal_result").innerHTML = "<i style=\"color: green\">Positive numbers: ".concat(positiveCount, "; Negative numbers: ").concat(negativeCount, "; Zeroes: ").concat(zeroCount, "; Even numbers: ").concat(evenCount, "; Odd numbers: ").concat(oddCount, "</i>");
+}
+
+function dayOfWeek() {
+  var dayIndex = 0;
+
+  do {
+    var day = void 0;
+
+    switch (dayIndex) {
+      case 0:
+        day = "Monday";
+        break;
+
+      case 1:
+        day = "Tuesday";
+        break;
+
+      case 2:
+        day = "Wednesday";
+        break;
+
+      case 3:
+        day = "Thursday";
+        break;
+
+      case 4:
+        day = "Friday";
+        break;
+
+      case 5:
+        day = "Saturday";
+        break;
+
+      case 6:
+        day = "Sunday";
+        break;
+    }
+
+    var confirmation = confirm(day + ". Do you want to see the next day?");
+
+    if (!confirmation) {
+      break;
+    }
+
+    dayIndex = (dayIndex + 1) % 7; //For example, if the current value of dayIndex is 6 (Saturday), the expression dayIndex = (dayIndex + 1) % 7 will evaluate to (6 + 1) % 7, which is 0. So, the value of dayIndex will be updated to 0, representing Sunday.
+  } while (true);
 }
--- 22-lesson/assets/js/js.js
+++ 22-lesson/assets/js/js.js
@@ -17,24 +17,24 @@ function userAge() {
 }
 
 
+// function symbol() {
+//   const age = document.getElementById("task_1_number").valueAsNumber;
+
+//   if(!isNaN(age) && age < 12 && age > 0 && age !== 0 ) {
+//     document.getElementById("task_1_result").innerHTML = `<i style="color: green">Child</i>`;
+//   } else if (age >= 12 && age < 18) {
+//     document.getElementById("task_1_result").innerHTML = `<i style="color: green">Teenager</i>`;
+//   } else if (age >= 18 && age < 60) {
+//     document.getElementById("task_1_result").innerHTML = `<i style="color: green">Adult</i>`;
+//   } else if (age >=60 && age <= 100) {
+//     document.getElementById("task_1_result").innerHTML = `<i style="color: green">Senior</i>`;
+//   } else {
+//     document.getElementById("task_1_result").innerHTML = `<i style="color: red">Enter value from 0 till 100</i>`;
+//     return ; 
+//   }
+// }
+
 function symbol() {
-  const age = document.getElementById("task_1_number").valueAsNumber;
-
-  if(!isNaN(age) && age < 12 && age > 0 && age !== 0 ) {
-    document.getElementById("task_1_result").innerHTML = `<i style="color: green">Child</i>`;
-  } else if (age >= 12 && age < 18) {
-    document.getElementById("task_1_result").innerHTML = `<i style="color: green">Teenager</i>`;
-  } else if (age >= 18 && age < 60) {
-    document.getElementById("task_1_result").innerHTML = `<i style="color: green">Adult</i>`;
-  } else if (age >=60 && age <= 100) {
-    document.getElementById("task_1_result").innerHTML = `<i style="color: green">Senior</i>`;
-  } else {
-    document.getElementById("task_1_result").innerHTML = `<i style="color: red">Enter value from 0 till 100</i>`;
-    return ; 
-  }
-}
-
-function symbol1() {
   const number = document.getElementById("task_2_number").valueAsNumber;
 
   switch(number) {
@@ -73,7 +73,44 @@ function symbol1() {
   }
 }
 
-function countDivider() {
+function sum() {
+  const num = document.getElementById("task_3_number").valueAsNumber;
+  const num1 = document.getElementById("task_3_number1").valueAsNumber;
+
+  let result = 0;
+
+  if (isNaN(num)) {
+    document.getElementById("task_3_result").innerHTML = `<i style="color: red">Enter value</i>`;
+    return false;
+  }
+
+  for(let i=num;i<=num1;i++) {
+    result += i;
+  }
+  document.getElementById("task_3_result").innerHTML = `<i style="color: red">${result}</i>`;
+}
+
+function commonDivisor() {
+  const num = document.getElementById("task_4_number").valueAsNumber;
+  const num1 = document.getElementById("task_4_number1").valueAsNumber;
+
+  let result = 0;
+
+  if (isNaN(num)) {
+    document.getElementById("task_4_result").innerHTML = `<i style="color: red">Enter a value</i>`;
+    return false;
+  }
+
+  for(let i = 1; i <= num && i <= num1; i++){
+    if(num % i === 0 && num1 % i === 0) {
+      result = i;
+    }
+  }
+  document.getElementById("task_4_result").innerHTML = `<i style="color: red">${result}</i>`;
+}
+
+
+function countDivisor() {
   const num = document.getElementById("task_5_number").valueAsNumber;
   let result = '';
 
@@ -89,4 +126,118 @@ function countDivider() {
   }
   result += num;
   document.getElementById("task_5_result").innerHTML = `<i style="color: red">${result}</i>`;
+}
+
+
+
+function palindrome() {
+  const num = document.getElementById("task_1_normal_number").valueAsNumber;
+  let result = '';
+
+  if (isNaN(num)) {
+    document.getElementById("task_1_normal_result").innerHTML = `<i style="color: red">Enter five-digit value</i>`;
+    return false;
+  }
+
+  if(Math.trunc(num / 10000) === num % 10 && Math.trunc((num / 1000) % 10) === Math.trunc((num % 100) / 10)) {
+    document.getElementById("task_1_normal_result").innerHTML = `<i style="color: green">The digit is palindrome</i>`;
+  } else {
+    document.getElementById("task_1_normal_result").innerHTML = `<i style="color: red">The digit is NOT palindrome</i>`;
+  }
+}
+
+
+function beforeDiscount() {
+  const amount = document.getElementById("task_2_normal_number").valueAsNumber;
+  let result = '';
+
+  if (isNaN(amount)) {
+    document.getElementById("task_2_normal_result").innerHTML = `<i style="color: red">Enter a value</i>`;
+    return false;
+  }
+
+  if (amount >= 200 && amount <= 300) {
+    result = (amount / (1 - 0.03)).toFixed(2);
+  } else if (amount > 300 && amount <= 500) {
+    result = (amount / (1 - 0.05)).toFixed(2);
+  } else if (amount > 500) {
+    result = (amount / (1 - 0.07)).toFixed(2);
+  }
+  document.getElementById("task_2_normal_result").innerHTML = `<i style="color: green">The amount without discount is ${result} USD</i>`;
+}
+
+
+function countNumbers() {
+  let i = 0;
+  let positiveCount = 0;
+  let negativeCount = 0;
+  let zeroCount = 0;
+  let evenCount = 0;
+  let oddCount = 0;
+
+  while (i < 10) {
+    const number = parseInt(prompt(`Enter number ${i + 1}:`));
+    
+    if (isNaN(number)) {
+      alert("Invalid number. Please try again.");
+      continue;
+    }
+    
+    if (number > 0) {
+      positiveCount++;
+    } else if (number < 0) {
+      negativeCount++;
+    } else {
+      zeroCount++;
+    }
+
+    if (number % 2 === 0) {
+      evenCount++;
+    } else {
+      oddCount++;
+    }
+
+    i++;
+  }
+
+  document.getElementById("task_4_normal_result").innerHTML = `<i style="color: green">Positive numbers: ${positiveCount}; Negative numbers: ${negativeCount}; Zeroes: ${zeroCount}; Even numbers: ${evenCount}; Odd numbers: ${oddCount}</i>`;
+}
+
+
+function dayOfWeek() {
+  let dayIndex = 0;
+
+  do {
+    let day;
+    switch (dayIndex) {
+      case 0:
+        day = "Monday";
+        break;
+      case 1:
+        day = "Tuesday";
+        break;
+      case 2:
+        day = "Wednesday";
+        break;
+      case 3:
+        day = "Thursday";
+        break;
+      case 4:
+        day = "Friday";
+        break;
+      case 5:
+        day = "Saturday";
+        break;
+      case 6:
+        day = "Sunday";
+        break;
+    }
+
+    const confirmation = confirm(day + ". Do you want to see the next day?");
+    if (!confirmation) {
+      break;
+    }
+    
+    dayIndex = (dayIndex + 1) % 7; //For example, if the current value of dayIndex is 6 (Saturday), the expression dayIndex = (dayIndex + 1) % 7 will evaluate to (6 + 1) % 7, which is 0. So, the value of dayIndex will be updated to 0, representing Sunday.
+  } while (true);
 }
