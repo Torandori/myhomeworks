08139a71b0b8072e6822589d9bd60f33d3afa1d6
Torandori <vaviktova@gmail.com>
2023-06-20 18:15:23 +0200

    Added les 27

--- 27-lesson/assets/js/dist/main.dev.js
+++ 27-lesson/assets/js/dist/main.dev.js
@@ -8,6 +8,26 @@ $(function () {
   $('#first-sec-slider').lightSlider({
     item: 1,
     loop: true,
-    enableDrag: true
+    enableDrag: true,
+    auto: true,
+    // pauseOnHover: true,
+    pause: 3000
   });
+  $('.second-sec-slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next')
+  });
+  $('.third-sec-slider').slick({
+    slidesToShow: 9,
+    slidesToScroll: 5,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next')
+  }); // $('.third-sec-slider').slick({
+  //   slidesToShow: 9,
+  //   slidesToScroll: 5,
+  //   prevArrow: $('.prev'),
+  //   nextArrow: $('.next'),
+  // });
 });
--- 27-lesson/assets/js/main.js
+++ 27-lesson/assets/js/main.js
@@ -11,7 +11,31 @@ $(function(){
     item: 1,
     loop: true,
     enableDrag: true,
+    auto: true,
+    // pauseOnHover: true,
+    pause: 3000,
   });
+
+  $('.second-sec-slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+  });
+
+  $('.third-sec-slider').slick({
+    slidesToShow: 9,
+    slidesToScroll: 5,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+  });
+
+  // $('.third-sec-slider').slick({
+  //   slidesToShow: 9,
+  //   slidesToScroll: 5,
+  //   prevArrow: $('.prev'),
+  //   nextArrow: $('.next'),
+  // });
 
 
 
