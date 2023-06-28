4f4e7867f87ed81d65a38b1ac917a68f51a13f8b
Torandori <vaviktova@gmail.com>
2023-06-28 02:46:48 +0200

    updates ex 2

--- exam-project2/assets/js/dist/main.dev.js
+++ exam-project2/assets/js/dist/main.dev.js
@@ -77,6 +77,53 @@ $(function () {
   // simple-gallery 
 
   var gallery = $('.gallery a').simpleLightbox({}); // #simple-gallery 
+  // SLICK SLIDER NEWS 
+
+  var slickSlider = $('.forth-sec-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    dots: true,
+    prevArrow: "<button type=\"button\" class=\"second-prev\">\n    <svg width=\"40\" height=\"40\" viewBox=\"0 0 40 40\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n    <circle opacity=\"0.5\" cx=\"20\" cy=\"20\" r=\"19.5\" stroke=\"white\"/>\n    <path d=\"M21.8984 17.25C21.8984 17.1797 21.8672 17.1172 21.8203 17.0703L21.4297 16.6797C21.3828 16.6328 21.3125 16.6016 21.25 16.6016C21.1875 16.6016 21.1172 16.6328 21.0703 16.6797L17.4297 20.3203C17.3828 20.3672 17.3516 20.4375 17.3516 20.5C17.3516 20.5625 17.3828 20.6328 17.4297 20.6797L21.0703 24.3203C21.1172 24.3672 21.1875 24.3984 21.25 24.3984C21.3125 24.3984 21.3828 24.3672 21.4297 24.3203L21.8203 23.9297C21.8672 23.8828 21.8984 23.8125 21.8984 23.75C21.8984 23.6875 21.8672 23.6172 21.8203 23.5703L18.75 20.5L21.8203 17.4297C21.8672 17.3828 21.8984 17.3125 21.8984 17.25Z\" fill=\"white\"/>\n    </svg>\n    </button>",
+    // better insert directly html markup of el here (svg possible)
+    nextArrow: "<button type=\"button\" class=\"second-next\">\n    <svg width=\"40\" height=\"40\" viewBox=\"0 0 40 40\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n    <circle opacity=\"0.5\" cx=\"20\" cy=\"20\" r=\"19.5\" stroke=\"white\"/>\n    <path d=\"M22.6484 20.5C22.6484 20.4375 22.6172 20.3672 22.5703 20.3203L18.9297 16.6797C18.8828 16.6328 18.8125 16.6016 18.75 16.6016C18.6875 16.6016 18.6172 16.6328 18.5703 16.6797L18.1797 17.0703C18.1328 17.1172 18.1016 17.1875 18.1016 17.25C18.1016 17.3125 18.1328 17.3828 18.1797 17.4297L21.25 20.5L18.1797 23.5703C18.1328 23.6172 18.1016 23.6875 18.1016 23.75C18.1016 23.8203 18.1328 23.8828 18.1797 23.9297L18.5703 24.3203C18.6172 24.3672 18.6875 24.3984 18.75 24.3984C18.8125 24.3984 18.8828 24.3672 18.9297 24.3203L22.5703 20.6797C22.6172 20.6328 22.6484 20.5625 22.6484 20.5Z\" fill=\"white\"/>\n    </svg>\n    </button>",
+    responsive: [{
+      breakpoint: 880,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }, {
+      breakpoint: 680,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }]
+  });
+  $('.second-prev').on('click', function () {
+    slickSlider.slick('slickPrev');
+  });
+  $('.second-next').on('click', function () {
+    slickSlider.slick('slickNext');
+  }); // #SLICK SLIDER NEWS 
+  // JSON JQ get news
+
+  function getNews() {
+    fetch('data/news.json').then(function (resp) {
+      return resp.json();
+    }).then(function (resp) {
+      console.log(resp);
+      var html = '';
+      resp.forEach(function (item) {
+        html += "\n          <div class=\"card\">\n            <div class=\"card-img\">\n              <img src=\"".concat(item.image, "\" alt=\"News picture\">\n            </div>\n            <div class=\"card-wrap-under-img\">\n              <h3 class=\"h3 montserrat\">").concat(item.title, "</h3>\n              <p>").concat(item.paragraph, "</p>\n              <div class=\"user\">\n                <div class=\"user-avatar\">\n                  <img src=\"").concat(item.userAvatar, "\" alt=\"User avatar\">\n                </div>\n                <div class=\"user-name montserrat\">\n                  <div>").concat(item.userName, "</div>\n                  <div>").concat(item.date, "</div>\n                </div>\n              </div>\n            </div>\n          </div>\n        ");
+      });
+      console.log(html);
+      $(".forth-sec-slider").append(html);
+    });
+  }
+
+  getNews(); // JSON jq get news
   // leaflet map
   // window.map_link.onclick = function(){
   //   window.getElementById('map').innerHTML = '';
@@ -97,11 +144,75 @@ $(function () {
       icon: circleIcon,
       title: 'Here we go'
     }).addTo(map).bindPopup('<b style="color: #7E5AFF">One World Trade Center<b>'); // .openPopup();
-  } // #leaflet map
-
+  }
 
   $('#map_link').click(function (event) {
     $('#map').html('');
     initMap();
+  }); // #leaflet map
+  // telegram send message 
+
+  window.contacts.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var errors = [];
+    var msg = '',
+        name = window.fullName.value,
+        email = window.email.value;
+
+    if (name === '') {
+      errors.push('Enter your name');
+    }
+
+    if (email === '') {
+      errors.push('Enter your email');
+    }
+
+    if (errors.length === 0) {
+      msg = "\n        <b>Name: </b>".concat(name, "\n        <b>Email: </b>").concat(email, "\n      ");
+      sendMessage(msg);
+    } else {
+      alert(errors.join(' '));
+    }
   });
+
+  function sendMessage(message) {
+    var apiToken, chatId, urlString, response, resp;
+    return regeneratorRuntime.async(function sendMessage$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            apiToken = "6124279978:AAHpO-C5Brpm2HgIpkDOpy7Zjkr5539aSMk";
+            chatId = "-1001969020828";
+            urlString = "https://api.telegram.org/bot".concat(apiToken, "/sendMessage?chat_id=").concat(chatId, "&text=").concat(encodeURIComponent(message), "&parse_mode=html");
+            _context.next = 5;
+            return regeneratorRuntime.awrap(fetch("https://api.telegram.org/bot".concat(apiToken, "/sendMessage"), {
+              method: 'post',
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({
+                chat_id: chatId,
+                text: message,
+                parse_mode: 'HTML'
+              })
+            }));
+
+          case 5:
+            response = _context.sent;
+            _context.next = 8;
+            return regeneratorRuntime.awrap(response.json());
+
+          case 8:
+            resp = _context.sent;
+            console.log(resp);
+
+          case 10:
+          case "end":
+            return _context.stop();
+        }
+      }
+    });
+  } // #telegram send message 
+
 });
--- exam-project2/assets/js/main.js
+++ exam-project2/assets/js/main.js
@@ -84,8 +84,94 @@ $(function(){
   });
   // #simple-gallery 
 
+  // SLICK SLIDER NEWS 
+
+  const slickSlider = $('.forth-sec-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    dots: true,
+    prevArrow: `<button type="button" class="second-prev">
+    <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle opacity="0.5" cx="20" cy="20" r="19.5" stroke="white"/>
+    <path d="M21.8984 17.25C21.8984 17.1797 21.8672 17.1172 21.8203 17.0703L21.4297 16.6797C21.3828 16.6328 21.3125 16.6016 21.25 16.6016C21.1875 16.6016 21.1172 16.6328 21.0703 16.6797L17.4297 20.3203C17.3828 20.3672 17.3516 20.4375 17.3516 20.5C17.3516 20.5625 17.3828 20.6328 17.4297 20.6797L21.0703 24.3203C21.1172 24.3672 21.1875 24.3984 21.25 24.3984C21.3125 24.3984 21.3828 24.3672 21.4297 24.3203L21.8203 23.9297C21.8672 23.8828 21.8984 23.8125 21.8984 23.75C21.8984 23.6875 21.8672 23.6172 21.8203 23.5703L18.75 20.5L21.8203 17.4297C21.8672 17.3828 21.8984 17.3125 21.8984 17.25Z" fill="white"/>
+    </svg>
+    </button>`, // better insert directly html markup of el here (svg possible)
+    nextArrow: `<button type="button" class="second-next">
+    <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle opacity="0.5" cx="20" cy="20" r="19.5" stroke="white"/>
+    <path d="M22.6484 20.5C22.6484 20.4375 22.6172 20.3672 22.5703 20.3203L18.9297 16.6797C18.8828 16.6328 18.8125 16.6016 18.75 16.6016C18.6875 16.6016 18.6172 16.6328 18.5703 16.6797L18.1797 17.0703C18.1328 17.1172 18.1016 17.1875 18.1016 17.25C18.1016 17.3125 18.1328 17.3828 18.1797 17.4297L21.25 20.5L18.1797 23.5703C18.1328 23.6172 18.1016 23.6875 18.1016 23.75C18.1016 23.8203 18.1328 23.8828 18.1797 23.9297L18.5703 24.3203C18.6172 24.3672 18.6875 24.3984 18.75 24.3984C18.8125 24.3984 18.8828 24.3672 18.9297 24.3203L22.5703 20.6797C22.6172 20.6328 22.6484 20.5625 22.6484 20.5Z" fill="white"/>
+    </svg>
+    </button>`,
+    responsive: [
+      {
+        breakpoint: 880,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 680,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+    ]
+  });
+
+  $('.second-prev').on('click', function(){
+    slickSlider.slick('slickPrev');
+  });
+  $('.second-next').on('click', function(){
+    slickSlider.slick('slickNext');
+  });
+
+  // #SLICK SLIDER NEWS 
+  // JSON JQ get news
+  function getNews(){
+    fetch('data/news.json')
+      .then(resp => resp.json())
+      .then(resp => {
+        console.log(resp);
+        let html = '';
+        resp.forEach(item => {
+          html += `
+          <div class="card">
+            <div class="card-img">
+              <img src="${item.image}" alt="News picture">
+            </div>
+            <div class="card-wrap-under-img">
+              <h3 class="h3 montserrat">${item.title}</h3>
+              <p>${item.paragraph}</p>
+              <div class="user">
+                <div class="user-avatar">
+                  <img src="${item.userAvatar}" alt="User avatar">
+                </div>
+                <div class="user-name montserrat">
+                  <div>${item.userName}</div>
+                  <div>${item.date}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        `;
+        });
+        console.log(html);
+        $(".forth-sec-slider").append(html);
+      })
+  }
+  getNews();
+  // JSON jq get news
+
+  
+  
+    
+
   
   // leaflet map
+
   // window.map_link.onclick = function(){
   //   window.getElementById('map').innerHTML = '';
   //   initMap();
@@ -107,13 +193,65 @@ $(function(){
     .bindPopup('<b style="color: #7E5AFF">One World Trade Center<b>')
     // .openPopup();
   }
-  // #leaflet map
   
   $('#map_link').click(function(event) {
     $('#map').html('');
     initMap();
   });
+  // #leaflet map
 
+  // telegram send message 
+
+  window.contacts.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const errors = [];
+    let msg = '',
+      name = window.fullName.value,
+      email = window.email.value;
+
+    if (name === '') {
+      errors.push('Enter your name');
+    }
+    if (email === '') {
+      errors.push('Enter your email');
+    }
+    if (errors.length === 0) {
+      msg = `
+        <b>Name: </b>${name}
+        <b>Email: </b>${email}
+      `;
+      sendMessage(msg);
+    } else {
+      alert(errors.join(' '));
+    }
+  });
+
+  async function sendMessage(message) {
+    const apiToken = "6124279978:AAHpO-C5Brpm2HgIpkDOpy7Zjkr5539aSMk";
+    const chatId = "-1001969020828";
+    let urlString = `https://api.telegram.org/bot${apiToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(message)}&parse_mode=html`;
+
+    const response = await fetch(`https://api.telegram.org/bot${apiToken}/sendMessage`, {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json' 
+      },
+      body: JSON.stringify({
+        chat_id: chatId,
+        text: message,
+        parse_mode: 'HTML',
+      })
+    });
+
+    const resp = await response.json();
+    console.log(resp);
+  }
+  // #telegram send message 
+
+
+  
 
   
 })
