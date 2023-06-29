503b3a7939d929408afac314a783bab688363d12
Torandori <vaviktova@gmail.com>
2023-06-29 21:29:14 +0200

    update ex2

--- exam-project2/assets/js/main.js
+++ exam-project2/assets/js/main.js
@@ -1,7 +1,7 @@
 $(function(){
   const lazyLoadInstance = new LazyLoad({});
 
-  // hamb 
+  // HAMB 
   function toggleMenu(){
     $('#hamb-btn').toggleClass("is-active");
     $('#mobile-sidebar').toggleClass("open");
@@ -34,7 +34,14 @@ $(function(){
       $('body').css('overflow', 'auto');
     };
   });
-  // #hamb 
+  // #HAMB 
+  
+  // ANIMATION 
+  const wow = new WOW({
+    animateClass: "animate__animated",
+  })
+  wow.init();
+  // #ANIMATION
 
   // HEADER SCROLLl
   $(window).scroll(function() {
@@ -57,7 +64,7 @@ $(function(){
   });
   //#HEADER SCROLL
 
-  // <!-- MOBMENU_SCROLL -->
+  // MOBMENU_SCROLL 
   $(window).scroll(function() {
     if($(window).scrollTop() > $('#first-sec').outerHeight() / 5){
       $('body').addClass('mobbar-fixed');
@@ -65,19 +72,10 @@ $(function(){
       $('body').removeClass('mobbar-fixed');
     }   
   })
-
-  // window.addEventListener('scroll', function(){
-  //   if(window.scrollY > document.getElementById("header-offset").offsetHeight){
-  //     document.body.classList.add("mobbar-fixed");
-  //   }else {
-  //     document.body.classList.remove("mobbar-fixed");
-  //   }
-  // })
-
-  // <!-- #MOBMENU_SCROLL -->
+  // #MOBMENU_SCROLL 
 
 
-  // lightslider 
+  // LIGHTSLIDER
   $('#first-sec-slider').lightSlider({
     item: 1,
     loop: true,
@@ -89,6 +87,7 @@ $(function(){
     verticalHeight: 800,
     keyPress: false,
     auto: true,
+    pauseOnHover: false,
     pause: 2500,
 
     onSliderLoad: function (el) {
@@ -113,18 +112,16 @@ $(function(){
       });
     }
   });  
+  // #LIGHTSLIDER
 
-  // #lightslider
-
-  // simple lightbox gallery 
+  // SIMPLE LIGHTBOX GALLERY
   const gallery = $('.gallery a').simpleLightbox({
     focus: false,
     enableKeyboard: false,
   });
-  // #simple-gallery 
+  // #SIMPLE LIGHTBOX GALLERY
 
   // SLICK SLIDER NEWS 
-
   const slickSlider = {
     slidesToShow: 3,
     slidesToScroll: 1,
@@ -136,7 +133,7 @@ $(function(){
     <path d="M21.8984 17.25C21.8984 17.1797 21.8672 17.1172 21.8203 17.0703L21.4297 16.6797C21.3828 16.6328 21.3125 16.6016 21.25 16.6016C21.1875 16.6016 21.1172 16.6328 21.0703 16.6797L17.4297 20.3203C17.3828 20.3672 17.3516 20.4375 17.3516 20.5C17.3516 20.5625 17.3828 20.6328 17.4297 20.6797L21.0703 24.3203C21.1172 24.3672 21.1875 24.3984 21.25 24.3984C21.3125 24.3984 21.3828 24.3672 21.4297 24.3203L21.8203 23.9297C21.8672 23.8828 21.8984 23.8125 21.8984 23.75C21.8984 23.6875 21.8672 23.6172 21.8203 23.5703L18.75 20.5L21.8203 17.4297C21.8672 17.3828 21.8984 17.3125 21.8984 17.25Z" fill="white"/>
     </svg>
     </button>`, // better insert directly html markup of el here (svg possible)
-    nextArrow: `<button type="button" class="second-next">
+    nextArrow: `<button type="button" class="second-next wow animate__flash" data-wow-delay=".5s">
     <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
     <circle opacity="0.5" cx="20" cy="20" r="19.5" stroke="white"/>
     <path d="M22.6484 20.5C22.6484 20.4375 22.6172 20.3672 22.5703 20.3203L18.9297 16.6797C18.8828 16.6328 18.8125 16.6016 18.75 16.6016C18.6875 16.6016 18.6172 16.6328 18.5703 16.6797L18.1797 17.0703C18.1328 17.1172 18.1016 17.1875 18.1016 17.25C18.1016 17.3125 18.1328 17.3828 18.1797 17.4297L21.25 20.5L18.1797 23.5703C18.1328 23.6172 18.1016 23.6875 18.1016 23.75C18.1016 23.8203 18.1328 23.8828 18.1797 23.9297L18.5703 24.3203C18.6172 24.3672 18.6875 24.3984 18.75 24.3984C18.8125 24.3984 18.8828 24.3672 18.9297 24.3203L22.5703 20.6797C22.6172 20.6328 22.6484 20.5625 22.6484 20.5Z" fill="white"/>
@@ -166,11 +163,9 @@ $(function(){
   $('.second-next').on('click', function(){
     slickSlider.slick('slickNext');
   });
-
   // #SLICK SLIDER NEWS 
   
   // JSON JQ get news
-
   function destroyCarousel() {
     if ($('.forth-sec-slider').hasClass('slick-initialized')) {
         $('.forth-sec-slider').slick('unslick');
@@ -218,8 +213,6 @@ $(function(){
         $('.forth-sec-slider').html(''); // now make html empty          
         $('.forth-sec-slider').html(html); // apply new data
         applySlider(); // apply slick slider again
-        // $(".forth-sec-slider").append(html);
-
         // lazy load for jquery script 
         const lazyLoadInstance = new LazyLoad({
           elements_selector: ".lazyload"
@@ -231,15 +224,8 @@ $(function(){
   }
   getNews();
   // JSON jq get news
-
   
-  // leaflet map
-
-  // window.map_link.onclick = function(){
-  //   window.getElementById('map').innerHTML = '';
-  //   initMap();
-  // };
-  
+  // LEAFLET MAP
   function initMap() {
     const map = L.map('map').setView([40.71275609925605, -74.01339694662713], 13);
     
@@ -254,17 +240,15 @@ $(function(){
     
     L.marker([40.71275609925605, -74.01339694662713], {icon: circleIcon, title: 'Here we go'}).addTo(map)
     .bindPopup('<b style="color: #7E5AFF">One World Trade Center<b>')
-    // .openPopup();
   }
   
   $('#map_link').click(function(event) {
     $('#map').html('');
     initMap();
   });
-  // #leaflet map
+  // #LEAFLET MAP
 
-  // telegram send message 
-
+  // TELEGRAM SEND MESSAGE 
   window.contacts.addEventListener('submit', function(event) {
     event.preventDefault();
     
@@ -322,11 +306,5 @@ $(function(){
       }
     }
   );
-
-  // #telegram send message 
-
-
-  
-
-  
+  // #TELEGRAM SEMD MESSAGE
 })
